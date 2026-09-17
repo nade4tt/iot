07e8359d0df856e8fd0f8059e2290e26cee90b09
nade4tt <denisnarat8@gmail.com>
2026-09-17 13:55:50 +0200

    Updates readdme, adds pico blink_simple

---
+++ pico/blink_simple/blink_simple.c
@@ -0,0 +1,19 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
+
+int main() {
+    stdio_init_all();
+    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+    gpio_init(LED_PIN);
+    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    while (true) {
+        gpio_put(LED_PIN, 1);
+        printf("LED ON");
+        sleep_ms(1000);
+
+        gpio_put(LED_PIN, 0);
+        printf("LED OFF");
+        sleep_ms(1000);
+    }
+}
